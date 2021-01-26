015d0c161a198c2ce209ab5127f37f4db866040d
Dane Owens <danereedhead@gmail.com>
2021-01-26 14:07:50 -0500

    Add project files.

---
+++ RuntimeIdentifierInferenceBug/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.RE2.Managed;
+using System;
+
+namespace RuntimeIdentifierInferenceBug
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Regex2.Test();
+            Console.WriteLine("Regex2.Test() --> success");
+            Console.ReadKey();
+        }
+    }
+}
